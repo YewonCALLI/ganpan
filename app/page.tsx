7ce6707fbe93e0af55391ed3cf25ed9a835e7075
KwonET <dlxl3652zz@gmail.com>
2024-09-08 11:38:07 +0900

    [init] next supabase setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import React from 'react';
+import Notes from './notes/page';
+const page = () => {
+    return (
+        <div>
+            메롱
+        </div>
+    );
+};
 
-export default async function Index() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
-}
+export default page;
