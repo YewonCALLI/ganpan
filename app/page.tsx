3f0900565d3af2a97333630dc2291c5018166d14
KwonET <dlxl3652zz@gmail.com>
2024-11-05 23:28:37 +0900

    [feat] page 기능 분리 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import dynamic from 'next/dynamic';
-
-import React, { useState } from 'react';
+var Aromanize = require("aromanize");
+import React, { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import '../app/app.css';
 import Image from 'next/image';
@@ -18,29 +18,93 @@ import GanpanImage from '@/components/GanpanImage';
 
 function Page1() {
   const [result, setResult] = useState([]);
-  const [inputText, setInputText] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [ganpanResult, setGanpanResult] = useState<ImageData[] | []>([])
+  const [enterPositions, setEnterPositions] = useState<Number[]>([])
+  const [averageWidth, setAverageWidth] = useState<Number>(0);
+  const [inputValue, setInputValue] = useState('');
+
   const router = useRouter();
+  const InputRef = useRef();
 
-  const maxChars = 15; 
+  const handleAverageWidth = (average: Number) => {
+    setAverageWidth(average)
+  }
+  const handleResetButton = () => {
+    InputRef.current.handleReset();
+  }
 
-  const handleInputChange = (e) => {
-    const value = e.target.value;
 
-    if (value.length <= maxChars) {
-      setInputText(value);
-      setErrorMessage('');
-    } else {
-      setErrorMessage(`최대 ${maxChars}자까지만 입력할 수 있습니다.`);
+  const fetchSearchedImages = async (userInput: string) => {
+    try {
+      const response = await fetch(`/api/get-search-image?input=${encodeURIComponent(userInput)}`, { method: 'GET' });
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      return data.results[randomIndex];
+    } catch (error) {
+      console.log(error);
+      return [];
     }
   };
 
-  const handleReset = () => {
-    setInputText('');
-    setResult([]);
-    setErrorMessage('');
-  };
+  const handleInputText = (
+    (newInput: string) => {
 
+      let processedList: string[] = [];
+      let newEnterPositions: number[] = [];
+      let currentWordLength = 0;
+      let wordLengths: number[] = [];
+
+      newInput.split("").forEach((input, index) => {
+        if (input === ' ') {
+          newEnterPositions.push(index);
+          // 현재 단어의 길이를 배열에 저장
+          wordLengths.push(currentWordLength);
+          // 단어 길이 리셋
+          currentWordLength = 0;
+        } else {
+          currentWordLength++;
+
+          if (/[a-zA-Z]/.test(input)) {
+            processedList.push(input.toUpperCase());
+          } else if (input === "닭") {
+            processedList.push("dalg");
+          } else {
+            processedList.push(Aromanize.romanize(input));
+          }
+        }
+        // 마지막 단어의 길이도 추가
+        if (currentWordLength > 0) {
+          wordLengths.push(currentWordLength);
+        }
+      });
+
+
+      // 모든 단어 길이의 평균 계산
+      const newAverage = wordLengths.length > 0
+        ? wordLengths.reduce((sum, length) => sum + length, 0) / wordLengths.length
+        : 0;
+
+      setAverageWidth(newAverage);
+      setEnterPositions(prevPositions => [...prevPositions, ...newEnterPositions]);
+
+      return processedList;
+    }
+  );
+  const handleGenerateButton = async (e: React.MouseEvent) => {
+    const processedInputs = handleInputText(inputValue);
+    const allResults = await Promise.all(processedInputs.map(fetchSearchedImages));
+    // 각 위치에 빈 ImageData 객체 삽입
+    enterPositions.forEach(position => {
+      allResults.splice(Number(position), 0, {
+        file_name: '',
+        public_url: ''
+      });
+    });
+    await setGanpanResult(allResults)
+  };
+  const handleInputChange = (input) => {
+    setInputValue(input);
+  }
   return (
     <>
       <Header />
@@ -60,35 +124,23 @@ function Page1() {
             <div className="input-area">
               <div className="top-container">
                 <div className="font1">
-                <p>최대 15자까지 가능해요.</p>
+                  <p>최대 15자까지 가능해요.</p>
                 </div>
-                <div className="font1-1">
+                <div className="font1-1" onClick={handleGenerateButton}>
                   <button>생성 ▶️</button>
                 </div>
                 <div className="font1">
                 </div>
-                <div className="font1-1">
+                <div className="font1-1" onClick={handleResetButton}>
                   <button>다시 ♽️</button>
                 </div>
               </div>
               <div className="top-container">
                 <div className='additional-container'>
-                <input
-                  type="text"
-                  value={inputText}
-                  placeholder='여기에 입력하세요'
-                  onChange={handleInputChange}
-                />
-                <div className="char-count">
-                  {inputText.length}/{maxChars}자
-                  {errorMessage && (
-                    <div className="error-message">{errorMessage}</div>
-                  )}
+                  <Input ref={InputRef} onInputChange={handleInputChange} />
                 </div>
-                </div>
-                <Image src={SampleImage}>
-                  {/* 이곳에 결과 이미지 */}
-                </Image>
+
+                <GanpanImage images={ganpanResult} averageWidth={averageWidth} />
                 {/* <Input onGanpanImage={handleGanpanImage} onAverageWidth={handleAverageWidth} />
                 <GanpanImage images={result} averageWidth={averageWidth} ></GanpanImage> */}
               </div>
